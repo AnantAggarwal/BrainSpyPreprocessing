9706eda7d63238ce77773cdeb5927ac8a8f53d27
anantaggarwal <anant.aggarwal026@gmail.com>
2025-06-29 15:26:26 +0530

    creating a new output dir in robex instead of replacing

--- preprocess.py
+++ preprocess.py
@@ -9,10 +9,12 @@ CURRENT_DIR = os.getcwd()
 ROBEX_DIR_PATH = os.path.join("BrainSpyPreprocessing", "ROBEX")
 ROBEX_CALL = os.path.join(CURRENT_DIR, ROBEX_DIR_PATH, "runROBEX.sh")
 
-def preprocessAndReplace(base_dir, command):
+def preprocessAndReplace(base_dir, command, name):
     for file in tqdm(glob.iglob(os.path.join(base_dir, "ADNI/**/**/**/**/*"))):
+        output_dir = os.path.relpath(file, BASE_DIR)
+        output_dir = os.path.join(name, output_dir)
         os.system(f"{command} {file} {file}")
 
 print("Running ROBEX Brain Extraction.......")
-preprocessAndReplace(BASE_DIR, ROBEX_CALL)
+preprocessAndReplace(BASE_DIR, ROBEX_CALL, "skull_stripped")
 print("Successfully ran ROBEX Brain Extraction.......")
