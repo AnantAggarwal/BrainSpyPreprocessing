278fdddd78d3465592d7be1227415e0dc98a1946
anantaggarwal <anant.aggarwal026@gmail.com>
2025-06-29 14:57:07 +0530

    corrected os.join to os.path.join

--- preprocess.py
+++ preprocess.py
@@ -9,7 +9,7 @@ ROBEX_DIR_PATH = "ROBEX"
 ROBEX_CALL = os.join(ROBEX_DIR_PATH, "runROBEX.sh")
 
 def preprocessAndReplace(base_dir, command):
-    for file in tqdm(glob.iglob(os.join(base_dir, "ADNI/**/**/**/**/*"))):
+    for file in tqdm(glob.iglob(os.path.join(base_dir, "ADNI/**/**/**/**/*"))):
         os.system(f"{command} {file} {file}")
 
 print("Running ROBEX Brain Extraction.......")
