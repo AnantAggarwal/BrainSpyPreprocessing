68a7fa5a0c663556e08005c8b6c8463e008232a5
anantaggarwal <anant.aggarwal026@gmail.com>
2025-06-29 17:27:39 +0530

    changed iglob to glob in iters

--- preprocess.py
+++ preprocess.py
@@ -10,13 +10,14 @@ CURRENT_DIR = os.getcwd()
 ROBEX_DIR_PATH = os.path.join("BrainSpyPreprocessing", "ROBEX")
 ROBEX_CALL = os.path.join(CURRENT_DIR, ROBEX_DIR_PATH, "runROBEX.sh")
 
-def preprocessAndReplace(base_dir, command, name):
-    iters = len(glob.iglob(os.path.join(base_dir, "ADNI/**/**/**/**/*")))
+def preprocessAndReplace(base_dir, commands, name):
+    iters = len(glob.glob(os.path.join(base_dir, "ADNI/**/**/**/**/*")))
     for file in tqdm(glob.iglob(os.path.join(base_dir, "ADNI/**/**/**/**/*")), total=iters):
         output_path = os.path.relpath(file, BASE_DIR)
         output_path = os.path.join(CURRENT_DIR, name, output_path)
         os.makedirs(os.path.dirname(output_path), exist_ok=True)
-        check_call([command, file, output_path], stdout=DEVNULL, stderr=STDOUT)
+        for command in commands:
+            check_call([command, file, output_path], stdout=DEVNULL, stderr=STDOUT)
 
 print("Running ROBEX Brain Extraction.......")
 preprocessAndReplace(BASE_DIR, ROBEX_CALL, "skull_stripped")
